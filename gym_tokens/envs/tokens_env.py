f4e0f544d5b3abadde0bb4e280966ed6afc1c338
Raymond Chua <ray.r.chua@gmail.com>
2019-12-30 09:22:04 +0800

    added comments to gym token env file

--- gym_tokens/envs/tokens_env.py
+++ gym_tokens/envs/tokens_env.py
@@ -4,29 +4,42 @@ from gym.utils import seeding
 import numpy as np
 import unittest
 
+
 class TokensEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
 
-	def __init__(self, seed=7, terminal=3):
+	def __init__(self, gamma, seed=7, terminal=3, fancy_discount=False):
+		'''
+		This is the constructor for the tokens env.
+		: param gamma (float): discount factor
+		: param seed (int): random seed value
+		: param terminal (int): max time step for the environment
+		: param fancy_discount (boolean): uses fancy discounting to compute reward 
+		'''
 
 		np.random.seed(seed)
 
 		self.num_actions = 3
 		# forward or backward in each dimension
 		self.action_space = spaces.Discrete(3)
-		self.observation_space = spaces.Box(low=np.array([-15, -15]), high=np.array([15, 15]), dtype=np.int64)
+		self.observation_space = spaces.Box(low=np.array([-terminal+1, -terminal+1]), high=np.array([terminal+1, terminal+1]), dtype=np.int64)
 		
 		# initial condition
 		self.state = np.zeros(2) #index 0: Nt, index 1: ht
+		self.gamma = gamma
 		self.reset()
 		self.terminal = terminal
-		
+		self.fancy_discount = fancy_discount
 
 	def step(self, action):
 		'''
-		# action = 0 as do nothing
-		# action = -1, left
-		# action = 1, right
+		The function takes in action and send that action to the environment. 
+		: param action :(integer consisting of [-1,0,1])
+		: return next state, reward, is_done (boolean) and none (extra info just in case)
+		Representation used for actions:
+			i) action = 0 as do nothing
+			ii) action = -1, go left
+			iii) action = 1, go right
 		'''
 
 		if action >1 or action <-1:
@@ -37,45 +50,39 @@ class TokensEnv(gym.Env):
 		ht_prev = self.state[1]
 		is_done = False
 
-		#Go left if prob is less than 0.5, go right otw. 
-		if coinToss <= 0.5:
-			Nt = Nt_prev - 1
+		#Go left if prob is less than 0.5, go right otherwise if in-game time-steps less than max time-step
+		if self.time_steps < self.terminal:
 
+			if coinToss <= 0.5:
+				Nt = Nt_prev - 1
+
+			else:
+				Nt = Nt_prev + 1
+
+		#When max time-step is reached, ensure that the final state observed (Nt) is the same as the previous
 		else:
-			Nt = Nt_prev + 1
+			Nt = Nt_prev
 
+		#Play action if all previous actions are waiting (action = 0). Else, preserve previous played actions
 		if ht_prev == 0:
 			ht = ht_prev + (self.time_steps+1) * action
 
 		else:
 			ht = ht_prev
 
+
+		#If in-game time has reached max time step, assign a reward value if the correct side (based on sign) is chosen.
 		if self.time_steps == self.terminal:
-			reward = self._indicator(self._sign(Nt_prev),self._sign(ht))
+			reward = self._indicator(self._sign(Nt),self._sign(ht))
 			next_state = self.reset()
 			is_done = True
 
-			return next_state, reward, is_done, self.time_steps
+			#fancy discounting reward is applied if initialised when the environment is constructed
+			if self.fancy_discount:
+				reward = self._fancy_discount_reward(reward)
 
 		else:
 
-			# coinToss = np.random.uniform()
-			# Nt_prev = self.state[0]
-			# ht_prev = self.state[1]
-			# is_done = False
-
-			# #Go left if prob is less than 0.5, go right otw. 
-			# if coinToss <= 0.5:
-			# 	Nt = Nt_prev - 1
-
-			# else:
-			# 	Nt = Nt_prev + 1
-
-			# if ht_prev == 0:
-			# 	ht = ht_prev + self.time_steps * action
-			# else:
-			# 	ht = ht_prev
-			
 			next_state = np.zeros(2,dtype=np.int64)
 			next_state[0] = Nt
 			next_state[1] = ht
@@ -83,20 +90,13 @@ class TokensEnv(gym.Env):
 
 			reward = 0
 			self.time_steps += 1
-
-
-		# else:
-		# 	reward = self._indicator(self._sign(Nt),self._sign(ht))
-		# 	next_state = self.reset()
-		# 	is_done = True
-
-			
-
 	
-		return next_state, reward, is_done, self.time_steps
+		return next_state, reward, is_done, None
+
+	def _fancy_discount_reward(self, reward):
+		return reward *(1+self.gamma*(1-np.absolute(ht)/self.terminal)/(1+self.gamma*(1-np.absolute(ht)/self.terminal)))
 
 	def _sign(self, num):
-
 		assert isinstance(num, np.int64)
 
 		if num < 0:
@@ -119,10 +119,6 @@ class TokensEnv(gym.Env):
 
 	def get_num_actions(self):
 		return self.num_actions
-
-
-
-
 
 	def reset(self):
 		self.state = np.zeros(2, dtype=np.int64)
