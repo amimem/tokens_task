2dd092a5fe1025b62aac56d0ed2e6eb676db9404
A M <nil.objc@gmail.com>
2020-04-18 17:18:51 -0400

    Added more comments to main.py

--- main.py
+++ main.py
@@ -77,9 +77,6 @@ def main():
 	# Set seed for all randomness sources
 	utils.seed(args.seed)
 
-	numNT = (args.height * 2) + 1 
-	numHT = (args.height * 2) + 1
-
 	if args.fast_block:
 		block_discount = 0.25
 
@@ -100,14 +97,20 @@ def main():
 
 	num_states = env.get_num_states()
 	num_actions = env.get_num_actions()
-	num_games_frames = args.height * args.games # Total time-steps
+	total_run_time_steps = args.height * args.games # Total time-steps
+
+	numNT = (args.height * 2) + 1  # -14 to 16
+	numHT = (args.height * 2) + 1 # -14 to 16
 
 	# model = lib.Q_Table(env.get_num_states(), env.get_num_actions(), (numNT, numHT), args.convg)
-	model = lib.Q_Table(numNT*numHT*(args.height+2), num_actions, (numNT, numHT, args.height), args.convg, args.height)
+	model = lib.Q_Table(numNT*numHT*(args.height+2), num_actions, (numNT, numHT, args.height), args.convg, args.height) 
+	#NOTE why the number of states is the way it is ? num_states x (height + 2)
 
 	if args.softmax:
 		policy = lib.SoftmaxPolicy()
-		tmp_track = lib.TemperatureTracker(args.tmp_start, args.tmp_final, args.tmp_games, policy)
+		tmp_track = lib.TemperatureTracker(args.tmp_start, args.tmp_final, args.tmp_games, policy) # tmp is changed from game to game
+		#FIXME change per episode or per time_step?
+		#BUG tracker is never called
 
 	if args.eps_soft:
 		policy = lib.EpsilonSoftPolicy()
@@ -121,7 +124,7 @@ def main():
 
 	else:
 		policy = lib.EpsilonGreedyPolicy()
-		eps_track = lib.EpsilonTracker(args.eps_start,args.eps_final, args.eps_games*args.height, policy)
+		eps_track = lib.EpsilonTracker(args.eps_start,args.eps_final, args.eps_games*args.height, policy) # args.eps_games*args.height is the number of total time_step for decreasing epsilon
 
 	if args.algo == 'sarsa': 
 		monkeyAgent = lib.SarsaAgent(policy, model, args.height)
@@ -129,7 +132,8 @@ def main():
 	elif args.algo == 'q-learning':
 		monkeyAgent = lib.QlAgent(policy, model, args.height)
 
-	lr_sched = lib.LRscheduler(args.lr, args.lr_final, args.games*args.height*10*0.8)
+	lr_sched = lib.LRscheduler(args.lr, args.lr_final, total_run_time_steps*10*0.8) 
+	#NOTE is there is reason that lr is not decreased to the final value during the experiment?
 
 	num_frames = status["num_frames"]
 	update = status["update"]
@@ -137,8 +141,8 @@ def main():
 	num_games_prevs = 0
 
 	start_time = time.time()
-	totalReturns = [] 
-	totalLoss = []
+	totalReturns = [] # Return per episode
+	totalLoss = [] # loss trajectory
 
 	state, game_time_step  = env.reset()
 	decisionTime = []
@@ -147,7 +151,9 @@ def main():
 
 	last_choice = 0
 
-	decisionTime = np.zeros(shape=((args.height*2)+1))
+	decisionTime = np.zeros(shape=((args.height*2)+1)) # histogram of decision times per episode
+	#NOTE why first 15 elements are always empty
+
 	# info = []
 
 	traj = []
@@ -160,15 +166,15 @@ def main():
 	numCorrectChoice = 0
 	numRecentCorrectChoice = []
 
-	while num_frames <= num_games_frames: 
+	while num_frames <= total_run_time_steps: 
 
 		traj.append(state[0].tolist())
 
 		if args.fancy_eps:
-			eps_track.set_eps(num_games)
+			eps_track.set_eps(num_games) # epsilon in fancy eps is changed from game to game
 
 		else:
-			eps_track.set_eps(num_frames)
+			eps_track.set_eps(num_frames) # otherwise it is changes timestep to timestep
 
 		action = monkeyAgent.get_actions(state, game_time_step)
 
@@ -180,38 +186,39 @@ def main():
 		# print('game_time_step: ', game_time_step)
 		# print('state: ', state)
 
-		lr = lr_sched.get_lr(num_frames)
+		lr = lr_sched.get_lr(num_frames) # learning rate is changed from timestep to timestep
 		
 		next_act = monkeyAgent.get_actions(next_state, game_time_step)
 		loss = model.get_TDerror(state, action, next_state, next_act, reward, args.gamma, is_done, args.algo)
 		converged = model.update_qVal(lr, state, action, loss)
-		totalLoss.append(loss)
+		totalLoss.append(loss) # loss trajectory
 
 		if is_done:
 			num_games+=1
-			totalReturns.append(reward)
+			totalReturns.append(reward) # reward per episode
 
 			if reward > 0:
 				numCorrectChoice += 1
 				numRecentCorrectChoice.append(1)
 			else:
-				numRecentCorrectChoice.append(0)
+				numRecentCorrectChoice.append(0) # binary value, correct choice or not per episode
 
 			lossPerEpisode.append(np.sum(totalLoss))
 			totalLoss = []
 
-			decision_step = model._augState(abs(next_state[1]))
-			decisionTime[decision_step-1] += 1
+			decision_step = model._augState(abs(next_state[1])) # taking abs means that decision step is always between 15 and 31
+			decisionTime[decision_step-1] += 1 # after each episode is done, one is added to the corresponding element in decision time,
+			# so after 100 episodes, we have a histogram of decision times
 
-			if abs(next_state[1]) == args.height+1:
-				last_choice += 1
+			if abs(next_state[1]) == args.height+1: # if we made no decision till the end
+				last_choice += 1 # last choice represents the number of episodes in which we waited until the end
 
-			choice_made.append(_sign(next_state[1]))
+			choice_made.append(_sign(next_state[1])) # these arays are updated after each episode, not after each timestep
 			correct_choice.append(_sign(next_state[0]))
-			finalDecisionTime.append(abs(next_state[1]))
+			finalDecisionTime.append(abs(next_state[1])) # Why next_state? because it is the latest state that we have and we don't update state until after the if-else condition
 			finalRewardPerGame.append(reward)
 
-			traj_group.append(traj)
+			traj_group.append(traj) # the list of all trajectories over all episodes
 			traj = []
 			next_state, game_time_step = env.reset()
 
@@ -222,17 +229,17 @@ def main():
 		state = next_state
 
 
-		if num_games > num_games_prevs and num_games % args.log_interval == 0:
+		if num_games > num_games_prevs and num_games % args.log_interval == 0: # if the game has not stpped and we moved an episode forward
 			duration = int(time.time() - start_time)
-			totalLoss_val = np.sum(lossPerEpisode)
-			totalReturn_val = np.sum(totalReturns)
+			totalLoss_val = np.sum(lossPerEpisode) # sum of all episodic losses
+			totalReturn_val = np.sum(totalReturns) # sum of all episodic returns
 
 			avg_loss = np.mean(lossPerEpisode[-1000:])
 			avg_returns = np.mean(totalReturns[-1000:])
 			recent_correct = np.mean(numRecentCorrectChoice[-1000:])
 
 			header = ["update", "frames", "Games", "duration"]
-			data = [update, num_frames, num_games, duration]
+			data = [update, num_frames, num_games, duration] # update and num_frames are +=15 ed
 
 			if args.softmax:
 				header += ["tmp", "lr", "last"]
